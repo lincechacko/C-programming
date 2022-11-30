dc62a85e3383d4ecce15727f4524f1a955fe9964
lincechacko <63118804+lincechacko@users.noreply.github.com>
2022-11-30 12:02:44 +0530

    Create str_tok.c

---
+++ emertxe/str_tok.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdio_ext.h>
+
+char *my_strtok(char str[], const char delim[]);
+int is_delim(char , char *);
+int main()
+{
+    char str[50], delim[50];
+    
+    printf("Enter the string  : ");
+    scanf("%s", str);
+    
+    __fpurge(stdout);
+ 
+    printf("Enter the delimeter : ");
+    scanf("\n%s", delim);
+    __fpurge(stdout);
+    
+    char *token = my_strtok(str, delim);
+    printf("Tokens :\n");
+    
+    while (token)
+    {
+        printf("%s\n", token);
+        token = my_strtok(str, delim);
+    }
+}
+
+
+char *my_strtok(char str[],const char delim[]){
+static int backup=0;
+int start;
+start=backup;
+int flag=0;
+if(str[start]=='\0'){
+return 0;
+}
+
+
+for(int i=start;str[i]!='\0';i++){
+for(int j=0;delim[j]!='\0';j++){
+if(str[i]==delim[j]){
+str[i]='\0';
+backup=i;
+backup++;
+
+for(int x=backup;str[x]!='\0';x++){
+for(int y=0;delim[y]!='\0';y++){
+if(str[x]==delim[y]){
+	flag=1;
+	break;
+}
+else{
+flag=0;
+}
+}
+if(flag==0){
+backup=x;	
+return str+start;
+}
+}
+
+}
+
+}
+backup++;
+}
+
+return str+start;
+}
